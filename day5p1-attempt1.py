671e94898156baac68e465cf6e0e9f248b9da55f
kenshin133 <bufmouse13@gmail.com>
2021-03-27 23:16:03 -0400

    working binary choice

--- day5p1-attempt1.py
+++ day5p1-attempt1.py
@@ -1,191 +1,97 @@
 import re
 
-
-
-
 '''
-    --- Day 4: Passport Processing ---
-    You arrive at the airport only to realize that you grabbed your North Pole Credentials instead of your passport. While these documents are extremely similar, North Pole Credentials aren't issued by a country and therefore aren't actually valid documentation for travel in most of the world.
+    --- Day 5: Binary Boarding ---
+    You board your plane only to discover a new problem: you dropped your boarding pass! You aren't sure which seat is yours, and all of the flight attendants are busy with the flood of people that suddenly made it through passport control.
     
-    It seems like you're not the only one having problems, though; a very long line has formed for the automatic passport scanners, and the delay could upset your travel itinerary.
+    You write a quick program to use your phone's camera to scan all of the nearby boarding passes (your puzzle input); perhaps you can find your seat through process of elimination.
     
-
-    Due to some questionable network security, you realize you might be able to solve both of these problems at the same time.
+    Instead of zones or groups, this airline uses binary space partitioning to seat people. A seat might be specified like FBFBBFFRLR, where F means "front", B means "back", L means "left", and R means "right".
     
-    The automatic passport scanners are slow because they're having trouble detecting which passports have all required fields. The expected fields are as follows:
+    The first 7 characters will either be F or B; these specify exactly one of the 128 rows on the plane (numbered 0 through 127). Each letter tells you which half of a region the given seat is in. Start with the whole list of rows; the first letter indicates whether the seat is in the front (0 through 63) or the back (64 through 127). The next letter indicates which half of that region the seat is in, and so on until you're left with exactly one row.
     
-    byr (Birth Year)
-    iyr (Issue Year)
-    eyr (Expiration Year)
-    hgt (Height)
-    hcl (Hair Color)
-    ecl (Eye Color)
-    pid (Passport ID)
-    cid (Country ID)
-    Passport data is validated in batch files (your puzzle input). Each passport is represented as a sequence of key:value pairs separated by spaces or newlines. Passports are separated by blank lines.
+    For example, consider just the first seven characters of FBFBBFFRLR:
     
-    Here is an example batch file containing four passports:
+    Start by considering the whole range, rows 0 through 127.
+    F means to take the lower half, keeping rows 0 through 63.
+    B means to take the upper half, keeping rows 32 through 63.
+    F means to take the lower half, keeping rows 32 through 47.
+    B means to take the upper half, keeping rows 40 through 47.
+    B keeps rows 44 through 47.
+    F keeps rows 44 through 45.
+    The final F keeps the lower of the two, row 44.
+    The last three characters will be either L or R; these specify exactly one of the 8 columns of seats on the plane (numbered 0 through 7). The same process as above proceeds again, this time with only three steps. L means to keep the lower half, while R means to keep the upper half.
     
-    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-    byr:1937 iyr:2017 cid:147 hgt:183cm
+    For example, consider just the last 3 characters of FBFBBFFRLR:
     
-    iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-    hcl:#cfa07d byr:1929
+    Start by considering the whole range, columns 0 through 7.
+    R means to take the upper half, keeping columns 4 through 7.
+    L means to take the lower half, keeping columns 4 through 5.
+    The final R keeps the upper of the two, column 5.
+    So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5.
     
-    hcl:#ae17e1 iyr:2013
-    eyr:2024
-    ecl:brn pid:760753108 byr:1931
-    hgt:179cm
-    
-    hcl:#cfa07d eyr:2025 pid:166559648
-    iyr:2011 ecl:brn hgt:59in
-
-    The first passport is valid - all eight fields are present. The second passport is invalid - it is missing hgt (the Height field).
-    
-    The third passport is interesting; the only missing field is cid, so it looks like data from North Pole Credentials, not a passport at all! Surely, nobody would mind if you made the system temporarily ignore missing cid fields. Treat this "passport" as valid.
-    
-    The fourth passport is missing two fields, cid and byr. Missing cid is fine, but missing any other field is not, so this passport is invalid.
-    
-    According to the above rules, your improved system would report 2 valid passports.
-    
-    Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
+    Every seat also has a unique seat ID: multiply the row by 8, then add the column. In this example, the seat has ID 44 * 8 + 5 = 357.
 '''
 
+def split(input, direction):
+    newInput=()
+    #newInput1=()
+    #for i in range(0,input):
+    #    newInput1 = newInput1 + (i ,)
 
-def validate(dict):
-    valid=0
-    requiredFields=["byr","iyr","eyr","hgt","hcl","ecl","pid"]
-    #print("byr should be between 1920 and 2002")
-    #print(dict['byr'])
-    #this could be faster with a loop that breaks when a field fails
-    if int(dict['byr']) < 1920 or int(dict['byr']) > 2002:
-        #print("this is not valid")
-        valid = 1
-    #print("should be 2010-2020")
-    #print(dict['iyr'])
-    if int(dict['iyr']) < 2010 or int(dict['iyr']) > 2020:
-        #print("this is not valid")
-        valid = 1 
-    if int(dict['eyr']) < 2020 or int(dict['eyr']) > 2030:
-        #print("this is not valid")
-        valid = 1
+    if direction == "B":
+        print("you chose upper")
+        temp=()
+        for i in range(int(len(input)/2),len(input)):
+           temp=temp + (input[i] ,) 
+        return(temp)
+    elif direction == "F":
+        print("you chose lower")
+        temp=()
+        for i in range(0,int(len(input)/2)):
+           if int(len(input)/2) > 1:     
+               temp = temp + (input[i] ,)
+           else:
+               temp = input[i] 
 
-    #for height
-    if "cm" in dict['hgt']:
-        temp=dict['hgt']
-        #print(temp)
-        temp=temp.strip('cm')
-        #print(temp)
-        if int(temp) < 150 or int(temp) > 193:
-          #  print("This is not valid")
-            valid = 1
+        return(temp)
+def makeRows(numberOfRows):
+    newInput1=()
+    for i in range(0,numberOfRows):
+        if numberOfRows > 1:
+            print(i)
+            print(numberOfRows)
+            newInput1 = newInput1 + (i ,)
+        else:
+            newInput1 = newInput1 + (i)
 
-    if "in" in dict['hgt']:
-        temp=dict['hgt']
-        #print(temp)
-        temp=temp.strip('in')
-        #print(temp)
-        if int(temp) < 59 or int(temp) > 76:
-         #   print("This is not valid")
-            valid = 1
+    return newInput1
 
-    #print(dict['hcl'])
-    if len(dict['hcl']) == 7:
-        if dict['hcl'][0] != '#':
-            valid = 1
-        validhex=('0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f')
-        for i in dict['hcl'][1:]:
-            if i not in validhex:
-                print(i + "is not in valid")
-                valid=1
-    if len(dict['hcl']) != 7:
-        valid = 1
 
-    eyeColors=('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth')
-    if dict['ecl'] not in eyeColors:
-        print("this is not valid")
-        print(dict['ecl'])
-        valid = 1
-    if len(dict['pid']) != 9:
-        print(dict['pid'])
-        print(len(dict['pid']))
-        print("this is not valid")
-        valid = 1
-    ##TODO validate based on the rules, each value
-     
-    
-    if valid==0:
-        return True
-
-##ABANDOMTODO normalize file.. perhaps replace newline with --- or something for easier viewing, organizing values, etc.
-
-#file=open("day4-input.sample2","r")
-file=open("day4-input","r")
+file=open("day5-input.sample","r")
+#file=open("day5-input","r")
 #this re regular expression removes the /n or whatever
 temp=file.readlines()
-#input=re.findall(r"\S+",file.read().split("\n\n"))
-#file=open("day3-input.sample","r")
-#143 file=open("day3-input","r")
-#144 #this re regular expression removes the /n or whatever
 
-#this seemed to work in light testing, if we can just have a loop that terminates whenever a blank space is found and reset. 
-string=""
-position=1
-passports=[]
 for i in temp:
-    if i != "\n":
-        if string == "":
-            string = i.strip('\n')
-        else:
-            string = string + " " + i.strip('\n')
-        if position == len(temp):
-#            print("LAST LINE")
-            passports.append(string)
-            string=""
-            position=0
-        else:
-            position=position+1
-        
-    else:
-#        print("DIE")
-        passports.append(string)
-        string=""
-        position=position+1
-#print(passports)
-for i in passports:
-    #print(i)
-    pass
+    print(i.strip("\n"))
+number=128
+rows=makeRows(128)
+print(rows)
 
-#data has been normalized at this point and we need to split the info into its specific parts
-# the following fields are required besides CID which can be set to optional
-#    byr (Birth Year) iyr (Issue Year) eyr (Expiration Year) hgt (Height) hcl (Hair Color) ecl (Eye Color) pid (Passport ID) cid (Country ID)
-#format of a passport looks like "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
-validPassports=[]
-for i in passports:
-    invalid=0
-    passport={}
-    fields=i.split(" ")
-    #print(fields)
-    for i in fields:
-        #i is now the key and value
-        key=i.split(':')[0]
-        value=i.split(':')[1]
-        passport[key]=value
-    #print(passport)
-    #you can get values using : passport["hcl"]
-    #print(passport["hcl"])
-    #validate all fields exist
-    #print(passport)
-    requiredFields=["byr","iyr","eyr","hgt","hcl","ecl","pid"]
-#    print(requiredFields)
-    for i in requiredFields:
-    #    print("checking if " + i + " is in " + str(passport))
-        if i in passport:
-#            print("so far so good")
-            pass
-        else:
-            invalid = 1
-    if invalid==0:
-        if validate(passport):
-            validPassports.append(passport)
-print(len(validPassports))
+split(rows,"B")
+remainingrows=split(rows,"B")
+print("pritingathing")
+print(remainingrows)
+remainingrows=split(remainingrows,"B")
+print(remainingrows)
+remainingrows=split(remainingrows,"B")
+print(remainingrows)
+remainingrows=split(remainingrows,"F")
+print(remainingrows)
+remainingrows=split(remainingrows,"B")
+print(remainingrows)
+remainingrows=split(remainingrows,"F")
+print(remainingrows)
+remainingrows=split(remainingrows,"F")
+print(remainingrows)
